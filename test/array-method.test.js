adfd47788b495c5a3c5f0cbe53125d522ae77b6e
Robyn Navarro <rnavarro182@gmail.com>
2018-07-13 10:24:25 -0700

    refactoring

--- test/array-method.test.js
+++ test/array-method.test.js
@@ -4,9 +4,7 @@ const { squareOddNums } = require('../lib/array-method');
 describe('square method', () => {
     const nums = [2, 3, 9, 12, 8, 5, 7];
 
-
     it('squares odd numbers and returns results greater than 24', () => {
-        const result = squareOddNums(nums);
-        assert.deepEqual(result, [81, 25, 49]);
+        assert.deepEqual(squareOddNums(nums), [81, 25, 49]);
     });
 });
